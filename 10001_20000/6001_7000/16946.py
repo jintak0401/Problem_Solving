62c52f7ecd15dcb2bc6a7374de4040e4eccbfd02
jintak0401 <jintak0401@naver.com>
2020-04-04 22:26:51 +0900

    BFS (벽 부수고 이동하기 4)

--- 10001_20000/6001_7000/16946.py
+++ 10001_20000/6001_7000/16946.py
@@ -10,20 +10,20 @@ def divide_area(arr):
     zone = 0
     for i in range(n):
         for j in range(m):
-            if arr[i][j] == '0' and (i, j) not in area:
+            if arr[i][j] == '0':
                 zone += 1
                 q = deque([(i, j)])
                 S[zone] = 1
-                area[(i, j)] = zone
+                arr[i][j] = zone
                 while q:
                     pt = q.popleft()
                     for dx, dy in d:
                         x, y = pt[0] + dx, pt[1] + dy
                         if 0 <= x < n and 0 <= y < m:
-                            if (x, y) not in area and arr[x][y] == '0':
+                            if arr[x][y] == '0':
                                 S[zone] += 1
                                 q.append((x, y))
-                                area[(x, y)] = zone
+                                arr[x][y] = zone
                 
 
 
@@ -31,8 +31,8 @@ def solve(arr, pt):
     zone = set()
     for dx, dy in d:
         x, y = pt[0] + dx, pt[1] + dy
-        if (x, y) in area:
-            zone |= {area[(x, y)]}
+        if 0 <= x < n and 0 <= y < m and arr[x][y] != '1':
+            zone |= {arr[x][y]}
 
     ret_val = 1
     for i in zone:
@@ -42,15 +42,13 @@ def solve(arr, pt):
 
 n, m = map(int, input().split())
 arr = []
-ans = [['0' for _ in range(m)] for _ in range(n)]
 for _ in range(n):
     arr.append(list(input().rstrip()))
 
 divide_area(arr)
 for i in range(n):
+    tmp = ['0'] * m
     for j in range(m):
         if arr[i][j] == '1':
-            ans[i][j] = solve(arr, (i, j))
-
-for i in range(len(ans)):
-    print(''.join(ans[i]))
+            tmp[j] = solve(arr, (i, j))
+    print(''.join(tmp))
